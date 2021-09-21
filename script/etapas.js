1c715d6d4d0f157d80ded22622284a1869e8d8e8
Alanys Ferreira <89104178+AlanysF@users.noreply.github.com>
2021-09-21 01:45:51 -0300

    Add files via upload

---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+            numero: '38111',
+            nome:'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+                {url:'../image/6.jpg', legenda:'Vereador', small:false}
+            ]
+        },
+        {
+            numero: '77222',
+            nome: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos:[
+                {url:'../image/7.jpg', legenda:'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Luna',
+            fotos: [
+                { url: '../image/8.jpg', legenda: 'Prefeito' },
+                { url: '../image/5.jpg', legenda: 'Vice-Prefeito', small: true }
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos: [
+                { url: '../image/9.jpg', legenda: 'Prefeito' },
+                { url: '../image/10.jpg', legenda: 'Vice-Prefeito', small: true }
+            ]
+        },
+    ]
+    }
+];
